d3f3128e356635143482d77fea5b714cf8074b24
Emotional_bug <1541373309@qq.com>
2024-04-20 00:13:14 +0800

    数组相关内置函数

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -10,7 +10,7 @@ import (
  */
 
 var builtins = map[string]*object.Builtin{
-	"len": {
+	"len": { //长度函数
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
@@ -19,14 +19,14 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
-			//case *object.Array:
-			//return &object.Integer{Value: int64(len(arg.Elements))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
 		},
 	},
-	"println": {
+	"println": { //打印函数 换行
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
 				fmt.Printf("%v\n", arg.Inspect())
@@ -34,7 +34,7 @@ var builtins = map[string]*object.Builtin{
 			return nil
 		},
 	},
-	"print": {
+	"print": { //打印函数
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
 				fmt.Printf("%v", arg.Inspect())
@@ -42,7 +42,7 @@ var builtins = map[string]*object.Builtin{
 			return nil
 		},
 	},
-	"input": {
+	"input": { //输入函数
 		Fn: func(args ...object.Object) object.Object {
 			var input string
 			_, err := fmt.Scanln(&input)
@@ -50,6 +50,81 @@ var builtins = map[string]*object.Builtin{
 				return newError("argument to `input` not supported, got %s", args[0].Type())
 			}
 			return &object.String{Value: input}
+		},
+	},
+	"first": { //取数组第一个元素
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
+		},
+	},
+	"last": { //取数组最后一个元素
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				return arr.Elements[length-1]
+			}
+
+			return NULL
+		},
+	},
+	"rest": { //取数组除最后一个元素
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+				return &object.Array{Elements: newElements}
+			}
+
+			return NULL
+		},
+	},
+	"push": { //向数组中添加元素
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			newElements := make([]object.Object, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+
+			return &object.Array{Elements: newElements}
 		},
 	},
 }
